058e54fda82df0ce04a8422b065cd6f95a23dcb0
betty <hyegyeong310@gmail.com>
2018-09-26 00:25:19 +0900

    클래스 상속 제거. self.place에 각 클래스의 인스턴스 지정

--- lotto_bowling_person/person.py
+++ lotto_bowling_person/person.py
@@ -7,7 +7,7 @@ class Lotto(object):
     def __init__(self, money):
         self.money = money
 
-    def lotto_menu(self):
+    def menu(self):
         pass
 
 
@@ -15,15 +15,23 @@ class Bowling(object):
     def __init__(self):
         pass
 
-    def bowling_menu(self):
+    def menu(self):
         pass
 
 
-class Person(Lotto, Bowling):
+class Home(object):
+    def __init__(self):
+        pass
+
+    def menu(self):
+        pass
+
+class Person(object):
 
     def __init__(self):
         self.money = 1000000
         self.menu = ["집으로 간다.", "로또사러 간다.", "볼링치러 간다."]
+        self.place = None
 
     def main_menu(self):
         print("무엇을 할까~~")
@@ -35,16 +43,30 @@ class Person(Lotto, Bowling):
         if 0 < choice < 4:
             if choice == 1:
                 print("집 도착!")
+                self.go_home()
             elif choice == 2:
                 print("로또사러 왔다.")
-                Lotto.lotto_menu(self)
+                self.go_lotto()
             else:
                 print("볼링치러 왔다.")
-                Bowling.bowling_menu(self)
+                self.go_bowling()
 
+            self.go()
         else:
             input("1~3 사이의 번호를 입력해주세요 enter->")
             return self.main_menu()
+
+    def go(self):
+        self.place.menu()
+
+    def go_home(self):
+        self.place = Home()
+
+    def go_lotto(self):
+        self.place = Lotto(self.money)
+
+    def go_bowling(self):
+        self.place = Bowling()
 
     def values_chk(self, str):
         while True:
