25b594ff78b2b275954d29dd619c75fafd9ddb5c
betty <hyegyeong310@gmail.com>
2018-09-30 21:58:31 +0900

    필요없는 파일 삭제. .gitignore 파일 제거. 파일 정리 후 나중에 올릴 것.

--- lotto_bowling_person/user_home.py
+++ lotto_bowling_person/user_home.py
@@ -1,46 +1,39 @@
 from enum import Enum
+import common_func
 import lotto
 import bowling
 import person
 
 
 class HomeMenu(Enum):
-    로또사러 = 1
-    볼링치러 = 2
-    밖으로 = 3
-    종료 = 4
+    Lotto = 1
+    Bowling = 2
+    Go_Outside = 3
+    Exit = 4
 
 
-class Home(object):
+class Home:
     def __init__(self, money):
         self.money = money
         self.place = None
+        self.common = common_func.CommonFunc()
 
     def sub_menu(self):
         print("집 도착!")
         for i in HomeMenu:
             print("{}. {}".format(i.value, i.name))
         choice = input("입력: ")
-        c2 = self.values_chk(choice)
-        if c2 == HomeMenu.로또사러.value:
+        c2 = self.common.values_chk(choice)
+        if c2 == HomeMenu.Lotto.value:
             self.place = lotto.Lotto(self.money)
-        elif c2 == HomeMenu.볼링치러.value:
+        elif c2 == HomeMenu.Bowling.value:
             self.place = bowling.Bowling(self.money)
-        elif c2 == HomeMenu.밖으로.value:
+        elif c2 == HomeMenu.Go_Outside.value:
             self.place = person.Person(self.money)
             self.place.sub_menu()
-        elif c2 == HomeMenu.종료.value:
+        elif c2 == HomeMenu.Exit.value:
             print("빠잉~!")
             pass
         else:
             print("1~4 사이의 수만 입력하세요.")
             return self.sub_menu()
-
-    def values_chk(self, str):
-        while True:
-            try:
-                num = int(str)
-                return num
-            except:
-                str = input("숫자만 입력해주세요: ")
-                return self.values_chk(str)
